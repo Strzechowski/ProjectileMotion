8c4ba34e30eadda163a66c63795ef7391b1a0b45
Strzechowski <rafal.strzechowski1@gmail.com>
2020-02-09 17:24:42 +0100

    Scale adjustment
    Scale can be one of 3 values now, it depends from the distance.
    distance < 10           -> scale = 100
    distance >10 && < 100   -> scale = 10
    distance > 100          -> scale = 4
    
    Before every projectile looked the same, beacause scale was counted
    explicitly for every motion and X axis was equal to the distance.
    Ball always landed at the end of the X axis.

--- Projectile_motion.py
+++ Projectile_motion.py
@@ -31,57 +31,66 @@ class ProjectileUI(QMainWindow):
         self.setMaximumSize(2*self.sizeX, self.sizeY)
 
 
-    def draw_X_axis(self, q_painter, ball_distance, x_start_point, y_start_point, scale):
-        q_painter.drawLine(x_start_point, y_start_point + 5, x_start_point + ball_distance * scale, y_start_point + 5)
-        fake_ball_distance_used_in_a_loop = int(ball_distance * 100)
-        one_part_of_fake_distance = int(fake_ball_distance_used_in_a_loop/25)
-        for x in range(0, fake_ball_distance_used_in_a_loop + 1, one_part_of_fake_distance):
-            x_value = x / 100
-            x_pixels = x_value * scale
+    def draw_X_axis(self, q_painter, axis_distance, x_start_point, y_start_point, scale):
+        q_painter.drawLine(x_start_point, y_start_point + 5, x_start_point + axis_distance * scale, y_start_point + 5)
+        fake_axis_distance_used_in_a_loop = int(axis_distance * 1000)
+        one_part_of_fake_distance = int(fake_axis_distance_used_in_a_loop/25)
+        for x in range(0, fake_axis_distance_used_in_a_loop + 1, one_part_of_fake_distance):
+            x_real_value = x / 1000
+            x_pixels = x_real_value * scale
             pixels_below_the_axis = 25
             q_painter.drawLine(x_start_point + x_pixels, y_start_point, x_start_point + x_pixels, y_start_point + 10)
 
+            # TO DO is it working ok now?
+            # TO DO if scale has 3 digit numbers, they seem to close to each other
             # two and three digit numbers should be moved few pixels more to the left, so they are directly under the ticks
             if x < 10:
-                q_painter.drawText(x_start_point + x_pixels - 3, y_start_point + pixels_below_the_axis, str(x_value))
+                q_painter.drawText(x_start_point + x_pixels - 3, y_start_point + pixels_below_the_axis, str(x_real_value))
             elif x < 100:
-                q_painter.drawText(x_start_point + x_pixels - 9, y_start_point + pixels_below_the_axis, str(x_value))
+                q_painter.drawText(x_start_point + x_pixels - 9, y_start_point + pixels_below_the_axis, str(x_real_value))
             else:
-                q_painter.drawText(x_start_point + x_pixels - 13, y_start_point + pixels_below_the_axis, str(x_value))
+                q_painter.drawText(x_start_point + x_pixels - 13, y_start_point + pixels_below_the_axis, str(x_real_value))
 
 
     def draw_balls(self, q_painter, ball_distance, x_start_point, y_start_point, scale):
-        fake_ball_distance_used_in_a_loop = int(ball_distance * 100)
+        # TO DO NEEDS EXPLANATION WHY * 1000
+        fake_ball_distance_used_in_a_loop = int(ball_distance * 1000)
         one_part_of_fake_distance = int(fake_ball_distance_used_in_a_loop/25)
         for x in range(0, fake_ball_distance_used_in_a_loop + 1, one_part_of_fake_distance):
-            x_value = x / 100
-            x_pixels = x_value * scale
-            y_value = x_value * math.tan(self.alfa_in_radians) - (self.g / (2 * (self.v0**2) * math.cos(self.alfa_in_radians)**2) * x_value**2)
-           # Z_test = self.v0**2 / self.g * math.sin(2 * self.alfa_in_radians)
-           # print('X pixels test #%s = %s' %(x, x_pixels))
+            x_real_value = x / 1000
+            x_pixels = x_real_value * scale
+            # TO DO formula needs explanation
+            y_value = x_real_value * math.tan(self.alfa_in_radians) - (self.g / (2 * (self.v0**2) * math.cos(self.alfa_in_radians)**2) * x_real_value**2)
             y_pixels = y_value * scale
             q_painter.drawEllipse(x_start_point + x_pixels - self.DotSize/2, y_start_point - y_pixels, self.DotSize, self.DotSize)
 
+
+    # TO DO explanation
+    def get_scale(self, distance):
+        if distance <= 10:
+            scale = 100
+        elif distance <= 100:
+            scale = 10
+        else:
+            scale = 4
+        return scale
 
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
         qp.setBrush(QBrush(Qt.red, Qt.SolidPattern))
-        qp.setPen(QPen(Qt.red))
+        qp.setPen(QPen(Qt.black))
 
-        self.v0 = 30
+        self.v0 = 45
+        # TO DO formula eplanation
         Z = self.v0**2 / self.g * math.sin(2 * self.alfa_in_radians)
-        print('Z distane = ' + str(Z))
+        pixel_scale = self.get_scale(Z)
+        axis_distance = 4 * 250 / pixel_scale
 
         PositionY = self.sizeY - (4*self.DotSize)
         PositionX = 2*self.DotSize
-        pixel_scale = 4 * 250 / Z
-        #scale = 4
 
-        print('scale = ' + str(pixel_scale))
-
-        qp.setPen(QPen(Qt.black))
-        self.draw_X_axis(qp, Z, PositionX, PositionY, pixel_scale)
+        self.draw_X_axis(qp, axis_distance, PositionX, PositionY, pixel_scale)
         self.draw_balls(qp, Z , PositionX, PositionY, pixel_scale)
 
 
